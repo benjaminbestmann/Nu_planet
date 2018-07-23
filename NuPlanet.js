b5f89832393d48c597165a3a696a2467d784bcb0
vivianamichel <demouser@MacBook-Pro.local>
2018-07-23 10:21:26 -0700

    Made login with google

---
+++ NuPlanet.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+  function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+}
--- main.py
+++ main.py
@@ -5,3 +5,23 @@ import json
 import urllib
 import urllib2
 from google.appengine.ext import ndb
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        loginTemplate = jinja_environment.get_template('templates/Login.html')
+        self.response.write(loginTemplate.render())
+
+    # function onSignIn(googleUser) {
+    #     var profile = googleUser.getBasicProfile();
+    #     console.log('ID: ' + profile.getId());
+    #     console.log('Name: ' + profile.getName());
+    #     console.log('Image URL: ' + profile.getImageUrl());
+    #     console.log('Email: ' + profile.getEmail());
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+])
