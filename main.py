29cb97913b9fe4247ea945bc1c45607e780b473a
BensMac <BensMac@Liquid.local>
2018-07-23 16:02:51 -0700

    Adding the map and search function

--- main.py
+++ main.py
@@ -5,18 +5,61 @@ import json
 import urllib
 import urllib2
 from google.appengine.ext import ndb
-from google.appengine.api import users
+from google.appengine.api import urlfetch
 
-jinja_environment = jinja2.Environment(
-    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)))
+class UserSearch(ndb.Model):
+    term = ndb.StringProperty(required=True)
+    count = ndb.IntegerProperty(required=True)
 
+def getGiphy():
+    search_term = "Paris, France"
+    params = {'api_key': 'AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w',
+              'q': search_term,}
+    url ="AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w&q=Eiffel+Tower,Paris+France"
+    api_url = '//www.google.com/maps/embed/v1/place?' + form_data
+    return api_url
+    # response = urllib2.urlopen(api_url)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        loginTemplate = jinja_environment.get_template('templates/Login.html')
-        self.response.write(loginTemplate.render())
-        
+
+        search_term = self.request.get('search') #what is being searched
+        if search_term: #if search_term does exist
+            lterm = search_term.lower()
+            # CREATE A KEY
+            key = ndb.Key('UserSearch', lterm) #the key functions is activated looking for
+            # READ DATABASE
+            search = key.get()
+            if not search:
+                #CREATE IF NOT THERE
+                search = UserSearch(
+                    key =key, count =0,
+                    term = search_term)
+            # UPDATE ACCOUNT
+            search.increase()
+            #  SAVE
+            #  ADD TO DATABASE
+                # if there's nothing in the array, it creates a new user and sets the page count to 1
+        else: #if search_term doesnt exist
+            search_term = "Seattle"
+
+        getGiphy()
+
+
+
+        # This stores the variables
+        # content = json.loads(response.read())
+        # This parses the data, turns it into a dictionary
+        # json.loads turns the string into a dictionary
+        locations = []
+
+        locations.append(search_term)
+
+        variables = {
+        # 'content': content,
+        'locations' : locations,
+        'search_term': search_term,
+        }
 
 app = webapp2.WSGIApplication([
-      ('/', MainPage),
-])
+    ('/', MainPage),])
