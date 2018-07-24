29af06e19cb350a12fa7ab8e33745b7ff666f24d
Maireen <demouser@Maireen-Espiritu.local>
2018-07-24 16:36:11 -0700

    Adding all the template stuff

--- main.py
+++ main.py
@@ -13,10 +13,21 @@ jinja_environment = jinja2.Environment(
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        loginTemplate = jinja_environment.get_template('templates/Login.html')
+        loginTemplate = jinja_environment.get_template('index.html')
         self.response.write(loginTemplate.render())
-        
+
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        signinTemplate = jinja_environment.get_template('old templates/Login.html')
+        self.response.write(signinTemplate.render())
+
+class UserPage(webapp2.RequestHandler):
+    def get(self):
+        userTemplate = jinja_environment.get_template('html5up-big-picture/user.html')
+        self.response.write(userTemplate.render())
 
 app = webapp2.WSGIApplication([
       ('/', MainPage),
+      ('/login', LoginPage),
+      ('/user', UserPage),
 ])
