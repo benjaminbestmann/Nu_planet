0d9fa3fc3dc58f7c2502e462106eae4549dd47be
BensMac <BensMac@Liquid.local>
2018-07-25 15:46:56 -0700

    Finally got the map system to work

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,37 +1,54 @@
+var map;
+
+function initialize() {
+  // Create a map centered in Pyrmont, Sydney (Australia).
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8666, lng: 151.1958},
+    zoom: 15
+  });
+
+  // Search for Google's office in Australia.
+  var request = {
+    location: map.getCenter(),
+    radius: '500',
+    query: 'Google Sydney'
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, callback);
+}
+
+// Checks that the PlacesServiceStatus is OK, and adds a marker
+// using the place ID and location from the PlacesService.
+function callback(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var marker = new google.maps.Marker({
+      map: map,
+      place: {
+        placeId: results[0].place_id,
+        location: results[0].geometry.location
+      }
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.onload = function(){
+/*window.onload = function(){
   main();
 }
 
 function main(){
 let searchButton = document.getElementById("searchButton");
-searchButton.addEventListener("click", grabUserInput);
+searchButton.addEventListener("click", createUrl);
 
 }
-function grabUserInput(){
+function createUrl(search){
   let user_input = document.getElementById("search");
   document.getElementById("map").src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w&q=" + user_input.value;
   console.log(document.getElementById("map").src);
   console.log(user_input.value);
 }
+*/
--- main.py
+++ main.py
@@ -4,25 +4,44 @@ import os
 import json
 import urllib
 import urllib2
+import logging
 from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
+
+jinja_environment = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(
+        os.path.dirname(__file__) + '/templates'))
 
 class UserSearch(ndb.Model):
     term = ndb.StringProperty(required=True)
     count = ndb.IntegerProperty(required=True)
 
-def getGiphy():
-    search_term = "Paris, France"
-    params = {'api_key': 'AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w',
-              'q': search_term,}
-    url ="AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w&q=Eiffel+Tower,Paris+France"
-    api_url = '//www.google.com/maps/embed/v1/place?' + form_data
-    return api_url
-    # response = urllib2.urlopen(api_url)
+class Location(object):
+    name = ""
+    Lng = 0
+    Lat = 0
+
+    def __init__(self, name, Lng, Lat):
+        self.name = name
+        self.Lng = Lng
+        self.Lat = Lat
+
+
+def make_location(name, Lng, Lat):
+    location = Location(name, Lng, Lat)
+    location.name = name
+    location.Lng = Lng
+    location.Lat = Lat
+    print(location.name)
+    print(location.Lng)
+    print(location.Lat)
+    return location
+
+
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-
+        make_location("Brussels", 100, 20)
         search_term = self.request.get('search') #what is being searched
         if search_term: #if search_term does exist
             lterm = search_term.lower()
@@ -36,30 +55,30 @@ class MainPage(webapp2.RequestHandler):
                     key =key, count =0,
                     term = search_term)
             # UPDATE ACCOUNT
-            search.increase()
-            #  SAVE
-            #  ADD TO DATABASE
                 # if there's nothing in the array, it creates a new user and sets the page count to 1
         else: #if search_term doesnt exist
             search_term = "Seattle"
 
-        getGiphy()
+        searchInfo = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%s&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyAQw6VVsR22oM5DCilaaXDEk5VOQKnLq9w" %search_term
 
-
-
+        # getGiphy()
         # This stores the variables
         # content = json.loads(response.read())
         # This parses the data, turns it into a dictionary
         # json.loads turns the string into a dictionary
         locations = []
+        # locations.append(search_term)
 
-        locations.append(search_term)
+# This finds all the info about the location you put in, the next step is to isolate the lat and lng
 
         variables = {
         # 'content': content,
         'locations' : locations,
         'search_term': search_term,
         }
+
+        template = jinja_environment.get_template('maps.html')
+        self.response.write(template.render(variables))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),])
